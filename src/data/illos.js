92fde6a35f4994e45cce28cb116679b731740396
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2023-01-20 23:12:55 -0500

    added data array

---
+++ src/data/illos.js
@@ -0,0 +1,140 @@
+export const illos = [
+    {
+      id: 1,
+      title: "Homecoming",
+      img_link: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/FEM5FWOA4RCNFG27LLTUCM2PVM.jpg",
+      text_color: "white",
+      background_color: "#343E64",
+      clock_image: "",
+      start_time: "0:00",
+      end_time: "2:00",
+      text:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet dui scelerisque, tempus dui non, blandit nulla. Etiam sed interdum est."
+    },
+    {
+      id: 2,
+      title: "LGBTQ",
+      img_link: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/Z4BWW6HGSZAYDJXUYI4FYWCDJU.png",
+      text_color: "white",
+      background_color: "#705970",
+      clock_image: "",
+      start_time: "2:00",
+      end_time: "4:00",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+      id: 3,
+      title: "NSOP",
+      img_link: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/AIDTE6P5SRC55G5C4SPM7GUV54.JPG",
+      text_color: "white",
+      background_color: "#A7939A",
+      clock_image: "",
+      start_time: "4:00",
+      end_time: "6:00",
+      text:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet dui scelerisque, tempus dui non, blandit nulla."
+    },
+    {
+      id: 4,
+      title: "SSFP",
+      img_link: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/ZGLLIPIBOVHPRMSBARH77X4ZNE.jpeg",
+      text_color: "#595050",
+      background_color: "#C8BFC3",
+      clock_image: "",
+      start_time: "6:00",
+      end_time: "8:00",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        id: 5,
+        title: "Homecoming",
+        img_link: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/FEM5FWOA4RCNFG27LLTUCM2PVM.jpg",
+        text_color: "#4D5266",
+        background_color: "#B7C8DB",
+        clock_image: "",
+        start_time: "8:00",
+        end_time: "10:00",
+        text:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet dui scelerisque, tempus dui non, blandit nulla. Etiam sed interdum est."
+      },
+      {
+        id: 6,
+        title: "LGBTQ",
+        img_link: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/Z4BWW6HGSZAYDJXUYI4FYWCDJU.png",
+        text_color: "#4D5266",
+        background_color: "#BCC3CA",
+        clock_image: "",
+        start_time: "10:00",
+        end_time: "12:00",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      },
+      {
+        id: 7,
+        title: "NSOP",
+        img_link: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/AIDTE6P5SRC55G5C4SPM7GUV54.JPG",
+        text_color: "#665C4D",
+        background_color: "#DED2C0",
+        clock_image: "",
+        start_time: "12:00",
+        end_time: "14:00",
+        text:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet dui scelerisque, tempus dui non, blandit nulla."
+      },
+      {
+        id: 8,
+        title: "SSFP",
+        img_link: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/ZGLLIPIBOVHPRMSBARH77X4ZNE.jpeg",
+        text_color: "#605151",
+        background_color: "#D3C6C6",
+        clock_image: "",
+        start_time: "14:00",
+        end_time: "16:00",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      },
+      {
+        id: 9,
+        title: "Homecoming",
+        img_link: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/FEM5FWOA4RCNFG27LLTUCM2PVM.jpg",
+        text_color: "#605151",
+        background_color: "#A79C9C",
+        clock_image: "",
+        start_time: "16:00",
+        end_time: "18:00",
+        text:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet dui scelerisque, tempus dui non, blandit nulla. Etiam sed interdum est."
+      },
+      {
+        id: 10,
+        title: "LGBTQ",
+        img_link: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/Z4BWW6HGSZAYDJXUYI4FYWCDJU.png",
+        text_color: "white",
+        background_color: "#857C87",
+        clock_image: "",
+        start_time: "18:00",
+        end_time: "20:00",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      },
+      {
+        id: 11,
+        title: "NSOP",
+        img_link: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/AIDTE6P5SRC55G5C4SPM7GUV54.JPG",
+        text_color: "white",
+        background_color:"#6E6980",
+        clock_image: "",
+        start_time: "20:00",
+        end_time: "22:00",
+        text:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet dui scelerisque, tempus dui non, blandit nulla."
+      },
+      {
+        id: 12,
+        title: "SSFP",
+        img_link: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/ZGLLIPIBOVHPRMSBARH77X4ZNE.jpeg",
+        text_color: "white",
+        background_color: "#5D586D",
+        clock_image: "",
+        start_time: "22:00",
+        end_time: "24:00",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      }
+  ];
