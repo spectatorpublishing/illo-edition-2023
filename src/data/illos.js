68ae650118719973545dbe2aa1c66231fde3c8a0
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2023-04-05 14:10:20 -0400

    added illo data

--- src/data/illos.js
+++ src/data/illos.js
@@ -14,140 +14,152 @@ import hour12 from "./../assets/clocks/hour12.png"
 export const illos = [
     {
       id: 1,
-      title: "Homecoming",
-      img_link: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/FEM5FWOA4RCNFG27LLTUCM2PVM.jpg",
+      location: "LeRoy Neiman Center for Print Studies",
+      img_link: "https://illoedition2023.s3.amazonaws.com/12am-2am+Mac+Jackson.png",
       text_color: "white",
       background_color: "#343E64",
       clock_image: hour1,
       start_time: "0:00",
       end_time: "2:00",
+      illustrator: "Mac Jackson, CC '24, Staff Illustrator",
       text:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet dui scelerisque, tempus dui non, blandit nulla. Etiam sed interdum est."
+        "After taking a printmaking class in Columbia's visual arts department, one qualifies to become a “shop monitor.” Monitors watch over the shop during the allotted “homework hours” that printmaking students frequent all week. Monitors are also given unlimited shop access, a huge advantage in getting intensive work done in a timely manner. As a print monitor, I have spent plenty of time in the shop after-hours with the occasional company of a graduate student or two. This illustration, a fabricated memory stylized as “footage,” shows a print in the process on the table at the forefront while the details of the studio's amenities dissolve in shadow."
     },
     {
       id: 2,
-      title: "LGBTQ",
-      img_link: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/Z4BWW6HGSZAYDJXUYI4FYWCDJU.png",
+      location: "Morton Williams",
+      img_link: "https://illoedition2023.s3.amazonaws.com/2am-4am+Kelsea+Petersen.png",
       text_color: "white",
       background_color: "#705970",
       clock_image: hour2,
       start_time: "2:00",
       end_time: "4:00",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      illustrator: "Kelsea Petersen, CC '25, Illustrations Deputy Editor",
+      text: "There are very few spaces around campus as liminal as Morton Williams at 2 a.m. Whether you find yourself getting snacks there after a night out or grabbing a red bull during a long night in, you will make some of the worst nutritional and financial decisions of your life here. Bathed in the ambient lighting of vaguely flickering fluorescent bulbs, you are guaranteed to be disoriented when you run into four or five side characters from your life in rapid succession. Dodge Hall's proximity to the grocery store has given me a particular love of the 2 a.m. energy drink run during long studio nights!"
     },
     {
       id: 3,
-      title: "NSOP",
-      img_link: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/AIDTE6P5SRC55G5C4SPM7GUV54.JPG",
+      location: "Diana Center Architecture Studio",
+      img_link: "https://illoedition2023.s3.amazonaws.com/4am-6am+Ji+Yoon+Sim.png",
       text_color: "white",
       background_color: "#A7939A",
       clock_image: hour3,
       start_time: "4:00",
       end_time: "6:00",
+      illustrator: "Ji Yoon Sim, BC '24, Illustrations Editor",
       text:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet dui scelerisque, tempus dui non, blandit nulla."
+        "As an architecture student, I have spent countless late nights in Diana Center's studio. It is safe to say that I am not the only one. Especially on the day before reviews, many students will camp out in the studio and work through the night into the morning to get their best work forward. As the sun rises and impending deadlines loom, the collective panic of all of the students is palpable, albeit sluggish given the late hour. In all honesty, there is nothing pleasant about an all-nighter. However, these all-nighters with classmates have created tight bonds and special moments I will never forget."
     },
     {
       id: 4,
-      title: "SSFP",
-      img_link: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/ZGLLIPIBOVHPRMSBARH77X4ZNE.jpeg",
+      location: "Absolute Bagels and Butler Library",
+      img_link: "https://illoedition2023.s3.amazonaws.com/6am-8am+Olya+Sukonrat.jpg",
       text_color: "#595050",
       background_color: "#C8BFC3",
       clock_image: hour4,
       start_time: "6:00",
       end_time: "8:00",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      illustrator: "Olya Sukonrat, CC '25, Illustrations Deputy Editor",
+      text: "Last semester, I developed an out-of-character routine: I would wake up at 6 a.m., get dressed for the day, and walk across the street to Absolute Bagels. In fact, I did this so often that the owners remembered my usual order, and we would briefly converse in our native Thai before I walked out. Though I spent the majority of my academic career identifying as a “night owl,” I found that waking up early gives me time to breathe and gather my thoughts before the day officially started. I also realized that morning was a great time to complete my many dense reading assignments; the libraries were empty then, and my mind was less foggy."
     },
     {
         id: 5,
-        title: "Homecoming",
-        img_link: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/FEM5FWOA4RCNFG27LLTUCM2PVM.jpg",
+        location: "Riverside Park",
+        img_link: "https://illoedition2023.s3.amazonaws.com/8am-10am+Chiara+Guigou.jpg",
         text_color: "#4D5266",
         background_color: "#B7C8DB",
         clock_image: hour5,
         start_time: "8:00",
         end_time: "10:00",
+        illustrator: "Chiara Guigou, GS '23, Staff Illustrator",
         text:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet dui scelerisque, tempus dui non, blandit nulla. Etiam sed interdum est."
+          "In a busy city like New York, it can be difficult to catch a break from the stress of school and work. However, I have found my remedy through daily morning runs in Riverside Park. With each step, the fresh air hits my face, and I am surrounded by nature. The river view glistens in the morning sunlight, providing me with the most beautiful view!"
       },
       {
         id: 6,
-        title: "LGBTQ",
-        img_link: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/Z4BWW6HGSZAYDJXUYI4FYWCDJU.png",
+        location: "The rink at Bryant Park",
+        img_link: "https://illoedition2023.s3.amazonaws.com/10am-12pm+Julian+Michaud.png",
         text_color: "#4D5266",
         background_color: "#BCC3CA",
         clock_image: hour6,
         start_time: "10:00",
         end_time: "12:00",
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        illustrator: "Julian Michaud, SEAS '24, Illustrations Deputy Editor",
+        text: "If you are trying to get away from campus and you have a free morning during the winter months, take the 1 train down to Bryant Park and enjoy its ice rink. It is one of the few rinks in the area that is free if you bring your own skates, and skating in the morning means you can avoid some of the evening rush. While you skate, numerous London plane trees encircle the rink, and the Grace Building towers behind them. Afterward, you can peruse the shops of the holiday market or grab a bite to eat before heading back to campus for your afternoon classes."
       },
       {
         id: 7,
-        title: "NSOP",
-        img_link: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/AIDTE6P5SRC55G5C4SPM7GUV54.JPG",
+        location: "Halal cart on 115th St",
+        img_link: "https://illoedition2023.s3.amazonaws.com/12pm-2pm+Xinyi+Niu.jpg",
         text_color: "#665C4D",
         background_color: "#DED2C0",
         clock_image: hour7,
         start_time: "12:00",
         end_time: "14:00",
+        illustrator: "Caroline Niu, CC '25, Staff Illustrator ",
         text:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet dui scelerisque, tempus dui non, blandit nulla."
+          "Whenever I am hungry for a quick lunch right before my afternoon classes, I go straight to the halal cart on 115th St. The aroma of the spices, chicken, beef, and lamb always attracts a long line of students around this time. As a student who is always low on meal swipes and looking for an affordable meal, the food carts, especially the halal cart on 115th St., have my whole heart."
       },
       {
         id: 8,
-        title: "SSFP",
-        img_link: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/ZGLLIPIBOVHPRMSBARH77X4ZNE.jpeg",
+        location: "Green umbrella table on campus",
+        img_link: "https://illoedition2023.s3.amazonaws.com/2pm-4pm+Yingjie+Wang.jpg",
         text_color: "#605151",
         background_color: "#D3C6C6",
         clock_image: hour8,
         start_time: "14:00",
         end_time: "16:00",
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        illustrator: "Yingjie Wang, SEAS '24, Senior Staff Illustrator",
+        text: "Mid-afternoon is the perfect time to get some studying done or to consider a nap between classes. When the weather is nice, one of my favorite study spots is at one of the green umbrella-adorned tables scattered around campus. These tables are notoriously hard to snag, but nothing beats the sunshine, the cool breeze, and doing work while listening to the rhythms of campus."
       },
       {
         id: 9,
-        title: "Homecoming",
-        img_link: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/FEM5FWOA4RCNFG27LLTUCM2PVM.jpg",
+        location: "Ferris Booth Commons dessert bar",
+        img_link: "https://illoedition2023.s3.amazonaws.com/4pm-6pm+Cindy+Jin.png",
         text_color: "#605151",
         background_color: "#A79C9C",
         clock_image: hour9,
         start_time: "16:00",
         end_time: "18:00",
+        illustrator: "Cindy Jin, CC '26, Staff Illustrator",
         text:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet dui scelerisque, tempus dui non, blandit nulla. Etiam sed interdum est."
+          " I cannot say I have a favorite dining hall, but the dessert bar at Ferris Booth Commons really makes a difference sometimes. What feeling can be better than finishing up a day of four classes after 4 p.m. and coming to Ferris to get a slice of cheesecake with an outrageous amount of sweetness? The fact that it is most definitely too sweet for my Asian taste buds turns it into a guilty pleasure."
       },
       {
         id: 10,
-        title: "LGBTQ",
-        img_link: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/Z4BWW6HGSZAYDJXUYI4FYWCDJU.png",
+        location: "Alfred Lerner Hall blacktop",
+        img_link: "https://illoedition2023.s3.amazonaws.com/6pm-8pm+Sadia+Sharif.png",
         text_color: "white",
         background_color: "#857C87",
         clock_image: hour10,
         start_time: "18:00",
         end_time: "20:00",
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        illustrator: "Sadia Sharif, CC '23, Senior Staff Illustrator",
+        text: "If you are looking for a peaceful, comfortable, and accessible place to study, then please do not even bother considering Alfred Lerner Hall on Wednesday nights. With the combination of Lion Dance's drums and cymbals and Sabor's speakers, the loud echoes throughout the building kick pretty much everyone out. As a Lion Dancer, I just hope that we're not disturbing Sabor too much with our deafening percussion instruments."
       },
       {
         id: 11,
-        title: "NSOP",
-        img_link: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/AIDTE6P5SRC55G5C4SPM7GUV54.JPG",
+        location: "Blue Gym",
+        img_link: "https://illoedition2023.s3.amazonaws.com/8pm-10pm+Cassie+Wang.jpg",
         text_color: "white",
         background_color:"#6E6980",
         clock_image: hour11,
         start_time: "20:00",
         end_time: "22:00",
+        illustrator: "Cassie Wang, CC '26, Staff Illustrator",
         text:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet dui scelerisque, tempus dui non, blandit nulla."
+          "Finishing dinner, many make their way through the frigid weather under the campus lights into the blast of warm air of the Butler Library doors. Finding a seat will probably take a bit, but eventually they will settle down, ready to take on another reading or a problem set. Some return to their dorm, ready to settle down for the day with some homework and music. And others head in the opposite direction, down into the Blue Gym of Dodge Fitness Center. Here, both undergraduate and graduate students help each other push out the ping pong tables and start setting them up to play. The sounds of laughter from conversing players, mismatched rhythms of table tennis balls hitting the table surface, rumbling of gym machines, and the cheerleading team in practice fill the Blue Gym."
       },
       {
         id: 12,
-        title: "SSFP",
-        img_link: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/ZGLLIPIBOVHPRMSBARH77X4ZNE.jpeg",
+        location: "",
+        img_link: "https://illoedition2023.s3.amazonaws.com/10pm-12pm+Liza+Evseeva.jpg",
         text_color: "white",
         background_color: "#5D586D",
         clock_image: hour12,
         start_time: "22:00",
         end_time: "24:00",
-        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        illustrator: "Liza Evseeva, CC '23, Senior Staff Illustrator",
+        text: "Yeah, artists party … sometimes. Once in a blue moon, you can find me, a real senior, at senior night on Wednesdays between the hours of 10 p.m. and midnight. This is also how I became friends with fellow Spectator staffer Abby Thayananthan, pictured here, through our shared love of dancing."
       }
   ];
